33b8bfd507aaba35adbf9968d23ad6aa4b6f83d4
William Taylor <willcodein@gmail.com>
2019-08-18 16:32:26 +0100

    added background image

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -12,7 +12,11 @@ const Text = styled.h4`
 `
 
 const Container = styled.div`
-    background: ${props => props.theme.colors.background};
+    // background: ${props => props.theme.colors.background};
+    background-color: ${props => props.theme.colors.darkBackground};
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23172a2c' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23224849' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%232e6665' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23398482' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%2345a29e' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
+    background-attachment: fixed;
+    background-size: cover;
     height: 100vh;
 `
 
