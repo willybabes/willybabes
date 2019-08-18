64faced5931404427a6c435f679b112569888a61
William Taylor <willcodein@gmail.com>
2019-08-18 14:46:44 +0100

    fixed build script

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import styled, { keyframes } from 'styled-components'
+
+const keyframesIlly = keyframes`
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity 1;
+    }
+    100% {
+        opacity 0;
+    }
+`
+
+const Text = styled.h4`
+    animation: ${keyframesIlly} 0.2s ease-in-out 0s infinite;
+`
 
 class App extends Component {
     render () {
         return (
             <div>
                 <Navbar />
-                Illybabes is a fitty
+                Illybabes is a fitty <Text>Option A all the time!</Text>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.scss', '.js', '.jsx'],
     },
     plugins: [
         new HtmlWebpackPlugin({
